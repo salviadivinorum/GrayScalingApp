5ecacb35cdadd2710c3846011eb39e393d1997c7
David Jaroš - DJ <david.jaros@chyron.com>
2025-03-13 16:42:21 +0100

    Add project files.

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Input;
+using System.Windows.Media;
+using GrayScalingApp.Model;
+
+namespace GrayScalingApp.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private ImageSource? iconBackground;
+        private string searchText;
+        private bool isGrayscaleToggled;
+
+        public MainWindowViewModel()
+        {
+            // TODO: Initialize the icon background.
+            // For now, IconBackground can be set when the image is loaded.
+            GrayscaleCommand = new RelayCommand(ExecuteGrayscale, CanExecuteGrayscale);
+            UpdateSearchText();
+        }
+
+        public ICommand GrayscaleCommand { get; }
+
+        public ImageSource? IconBackground
+        {
+            get => iconBackground;
+            set
+            {
+                if (iconBackground != value)
+                {
+                    iconBackground = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                if (searchText != value)
+                {
+                    searchText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsGrayscaleToggled
+        {
+            get => isGrayscaleToggled;
+            set
+            {
+                if (isGrayscaleToggled != value)
+                {
+                    isGrayscaleToggled = value;
+                    OnPropertyChanged();
+                    UpdateSearchText();
+                }
+            }
+        }
+
+        private bool CanExecuteGrayscale(object parameter)
+        {
+            // Enable the command by default.
+            return true;
+        }
+
+        private void ExecuteGrayscale(object parameter)
+        {
+            // TODO: Add gray scaling logic here.
+        }
+
+        private void UpdateSearchText()
+        {
+            SearchText = IsGrayscaleToggled ? "Grayscale is ON" : "Grayscale is OFF";
+        }
+    }
+}
---
+++ ViewModel/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace GrayScalingApp.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object>? canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
